d43433a6dcac68a115f6cb2d3d981fdefe6d42e7
Vladimir Iukhimenko <vlad.imenko@yandex.ru>
2020-07-23 15:29:13 +0300

    init commit

---
+++ src/main/java/ru/pet/shelter/ShelterApplication.java
@@ -0,0 +1,13 @@
+package ru.pet.shelter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShelterApplication.class, args);
+    }
+
+}
