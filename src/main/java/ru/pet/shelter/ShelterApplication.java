80bc681df74a9c46092d08f52ac842a5aee63412
Vladimir Iukhimenko <vlad.imenko@yandex.ru>
2020-09-29 11:05:25 +0300

    Security config v4.1

--- src/main/java/ru/pet/shelter/ShelterApplication.java
+++ src/main/java/ru/pet/shelter/ShelterApplication.java
@@ -1,13 +1,18 @@
 package ru.pet.shelter;
 
 import com.github.cloudyrock.spring.v5.EnableMongock;
+import io.swagger.v3.oas.annotations.security.SecuritySchemes;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import java.util.List;
 
 @SpringBootApplication
 @Slf4j
@@ -27,8 +32,18 @@ public class ShelterApplication {
 
     @Bean
     public OpenAPI customOpenApi() {
-        return new OpenAPI().info(new Info().title("My API").version("1.0"));
+        return new OpenAPI()
+                .info(new Info().title("My API").version("1.0"))
+                .components(new Components().addSecuritySchemes("OAuth2", securityScheme()))
+                .addSecurityItem(new SecurityRequirement().addList("OAuth2"));
     }
 
+    private SecurityScheme securityScheme() {
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.OAUTH2)
+                .description("This is Oauth2 scheme")
+                .flows(new OAuthFlows().implicit(new OAuthFlow()
+                        .authorizationUrl("https://oauth.vk.com/authorize")));
+    }
 
 }
--- src/main/java/ru/pet/shelter/config/ResourceServerConfig.java
+++ src/main/java/ru/pet/shelter/config/ResourceServerConfig.java
@@ -16,7 +16,24 @@ public class ResourceServerConfig {
     private final AuthenticationManager authenticationManager;
     private final SecurityContextRepository securityContextRepository;
 
-    private static final String[] SECURED_PATTERN = new String[] {"/boom"};
+    private static final String[] SECURED_PATTERN = new String[] {
+            "/chip/**",
+            "/cat/**",
+            "/dog/**",
+            "/description/**",
+            "/photo/**",
+            "/request/**",
+            "/pass-vaccine/**",
+            "/passport/**",
+            "/shelter/**"
+    };
+
+    private static final String[] NOT_SECURED_PATTERN = new String[] {
+            "/v3/api-docs/**",
+            "/swagger-resources/**",
+            "/swagger-ui.html**",
+            "/webjars/**",
+    };
 
 
     @Autowired
@@ -29,15 +46,16 @@ public class ResourceServerConfig {
     @Bean
     public SecurityWebFilterChain configure(ServerHttpSecurity http) {
         //http.exceptionHandling().authenticationEntryPoint(new AuthExceptionEntryPoint());
+        http.cors().disable();
+        http.csrf().disable();
 
         http
-                .cors().disable()
-                .csrf().disable()
+                .httpBasic().disable()
                 .authenticationManager(authenticationManager)
                 .securityContextRepository(securityContextRepository)
                 .authorizeExchange()
                 .pathMatchers(SECURED_PATTERN).authenticated()
-                .anyExchange().permitAll();
+                .pathMatchers(NOT_SECURED_PATTERN).permitAll();
         return http.build();
     }
 }
